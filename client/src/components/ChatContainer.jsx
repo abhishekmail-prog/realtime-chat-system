4a31620ed2539d5d8fedb2c4a0c27c129885815a
abhishekmail-prog <abhishekmail0301@gmail.com>
2026-06-16 19:13:50 +0530

    Add send button and image attachment and designed chat inputs

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -27,7 +27,7 @@ const ChatContainer = ({selectedUser, setSelectedUser}) => {
 				<img src = {assets.help_icon} alt = "" className = 'max-md:hidden max-w-5' />
 			</div>
 			{/*---- chat area ----*/}
-			<div className = 'flex flex-col h-[calc(100%-120px)] overflow-y-scroll p-3 pb-6'>
+			<div className = 'flex flex-col h-[calc(100%_-_120px)] overflow-y-scroll p-3 pb-6'>
 				{messagesDummyData.map((msg, index)=>(
 					<div key = {index} className = {`flex items-end gap-2 justify-end 
 					           ${msg.senderId !== '680f50e4f10f3ccd28382ecf9' && 'flex-row-reverse'}`}>
@@ -52,18 +52,26 @@ const ChatContainer = ({selectedUser, setSelectedUser}) => {
 			</div>
 
 {/*---- bottom area ----*/}
-		<div className = 'absolute bottom-0 left-0 right-0 flex-items-center gap-3 p-3'>
-			<div>
-				<input type = "text" placeholde = "Send a message" />
-				<input type = "file" id = 'image' accept = 'image/png, image/jpeg' hidden />
-			</div>
+		<div className = 'absolute bottom-0 left-0 right-0 flex items-center gap-3 p-3'>
+
+			<div className='flex-1 flex items-center bg-white/10 backdrop-blur-md border border-white/10 px-4 py-1 rounded-full'>
+  				<input type='text' placeholder='Type a message...' 
+  					className='flex-1 bg-transparent text-sm p-3 outline-none text-white placeholder-gray-400'/>
+
+  				<input type='file' id='image' accept='image/png, image/jpeg' hidden />
+
+  				<label htmlFor='image'>
+    			<img src={assets.gallery_icon} alt='' className='w-5 mr-2 cursor-pointer opacity-70 hover:opacity-100'/>
+		  		</label>
+		  	</div>
+			<img src = {assets.send_button} alt = "" className= "w-7 cursor-pointer" />
 		</div>
 		</div>
 	) : (
 		<div className = 'flex flex-col items-center justify-center gap-2 text-gray-500 
 			              bg-white/10 max-md:hidden'>
 			<img src = {assets.logo_icon} className='max-w-16' alt = "" />
-			<p className = 'text-lg font-medium text-white'>Chat anytime, anywhere/</p>
+			<p className = 'text-lg font-medium text-white'>Chat anytime, anywhere</p>
 		</div>
 	)
 }
