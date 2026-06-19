884743204fe6640a8509fc645bb8d234c6d46d58
abhishekmail-prog <abhishekmail0301@gmail.com>
2026-06-19 18:39:02 +0530

    Add signup structure for name and email

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -4,6 +4,12 @@ import assets from '../assets/assets'
 const LoginPage = () => {
 
 	const [currState, setCurrState] = useState("Sign up")
+	const [fullName, setFullName] = useState("")
+	const [email, setEmail] = useState("")
+	const [password, setPassword] = useState("")
+	const [bio, setBio] = useState("")
+	const [isDataSubmitted, setIsDataSubmitted] = useState(false)
+	
 
 	return (
 		<div className = 'min-h-screen bg-cover bg-center flex items-center justify-center
@@ -18,6 +24,21 @@ const LoginPage = () => {
 			    	{currState}
 			    	<img src = {assets.arrow_icon} alt="" className = 'w-5 cursor-pointer' />
 			    </h2>
+			    {currState === "Sign up" && !isDataSubmitted && (
+			    	<input  onChange={(e)=>setFull(e.target.value)} value = {email}
+			    	 type = "text" className = 'p-2 border border-gray-500 rounded-md 
+			    		focus:outline-none' placeholder="Full Name" required />	
+			    )}
+
+			    {!isDataSubmitted && (
+			    	<>
+			    	<input onChange={(e)=>setEmail(e.target.value)} value = {email} 
+			    		type = "email" placeholder = 'Email Address' required 
+			    		className = 'p-2 border border-gray-500 rounded-md focus:outline-none
+			    		focus:ring-2 focus:ring-indigo-500'/>
+			    	</>
+			    )}
+
 			</form>		
 
 		</div>
