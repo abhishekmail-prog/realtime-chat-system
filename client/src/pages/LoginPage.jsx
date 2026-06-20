8796554519131a0a7b9fb216980fab24eaeb18d8
abhishekmail-prog <abhishekmail0301@gmail.com>
2026-06-20 18:49:47 +0530

    Fix: input box to background color to avoid visible collision

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -12,48 +12,49 @@ const LoginPage = () => {
 	
 
 	return (
-		<div className = 'min-h-screen bg-cover bg-center flex items-center justify-center
-			gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl'>
+		<div className='min-h-screen flex items-center justify-center gap-8 
+			sm:justify-evenly max-sm:flex-col'>
 			{/*---- left ----*/}
 			<img src = {assets.logo_big} alt = "" className = 'w-[min(30vw,250px)]' />
 			{/*---- right ----*/}
 
-			<form className = 'border-2 bg-white/8 text-white border-gray-500 p-6 flex
-							   flex-col gap-6 rounded-lg shadow-lg' >
+			<form className='border-2 bg-black text-white border-gray-500 p-6 flex flex-col 
+				gap-6 rounded-lg shadow-lg z-50 relative'>
 			    <h2 className = 'font-medium text-2xl flex justify-between items-center' >
 			    	{currState}
 			    	<img src = {assets.arrow_icon} alt="" className = 'w-5 cursor-pointer' />
 			    </h2>
 			    {currState === "Sign up" && !isDataSubmitted && (
 			    	<input  onChange={(e)=>setFullName(e.target.value)} value = {fullName}
-			    	 type = "text" className = 'p-2 border border-gray-500 rounded-md 
-			    		focus:outline-none' placeholder="Full Name" required />	
+			    	 type = "text" className = 'p-2 border border-gray-500 rounded-md bg-transparent text-white 
+			    		focus:outline-none focus:ring-2 focus:ring-indigo-500' placeholder="Full Name" required />	
 			    )}
 
 			    {!isDataSubmitted && (
 			    	<>
 			    	<input onChange={(e)=>setEmail(e.target.value)} value = {email} 
 			    		type = "email" placeholder = 'Email Address' required 
-			    		className = 'p-2 border border-gray-500 rounded-md focus:outline-none
-			    		focus:ring-2 focus:ring-indigo-500'/>
+			    		className = 'p-2 border border-gray-500 rounded-md bg-transparent text-white 
+			    		focus:outline-none focus:ring-2 focus:ring-indigo-500'/>
 
 			    	<input onChange={(e)=>setPassword(e.target.value)} value = {password} 
 			    		type = "password" placeholder = 'Password' required 
-			    		className = 'p-2 border border-gray-500 rounded-md focus:outline-none
-			    		focus:ring-2 focus:ring-indigo-500'/>
+			    		className = 'p-2 border border-gray-500 rounded-md bg-transparent text-white 
+			    		focus:outline-none focus:ring-2 focus:ring-indigo-500'/>
 			    	</>
 			    )}
 
 			    {currState === "Sign up" && isDataSubmitted && (
 			    		<textarea onChange={(e)=>setBio(e.target.value)} value= {bio}
-			    			rows={4} className = 'p-2 border-gray-500 rounded-md focus:outline-none
-			    			focus:ring-2 focus:ring-indigo-500' placeholder = 'provide a short bio...'
+			    			rows={4} className = 'p-2 border-gray-500 rounded-md bg-transparent text-white
+			    			focus:outline-none focus:ring-2 focus:ring-indigo-500' 
+			    			placeholder = 'provide a short bio...'
 			    			required >
 			    		</textarea>
 			    	)
 			    }
 
-			    <button type = 'submit' className = 'py-3 bg-gradiant-to-r from-purple-400 to-violet-600
+			    <button type = 'submit' className = 'py-3 bg-gradient-to-r from-purple-400 to-violet-600
 			    	text-white rounded-md cursor-pointer' >
 			    	{currState === "Sign up" ? "Create Account" : "Login Now"}
 			    </button>
