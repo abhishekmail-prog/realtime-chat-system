4f640935c4f45a0e7ab9acda11ced36471766adf
abhishekmail-prog <abhishekmail0301@gmail.com>
2026-06-22 18:49:21 +0530

    Add profile form submission in profile page

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -9,13 +9,18 @@ const ProfilePage = () => {
 	const [name, setName] = useState("Martin Johnson")
 	const [bio, setBio] = useState("Hi Everyone, I am using QuickChat")
 
+	const handleSubmit = async(e)=>{
+		e.preventDefault();
+		navigate('/')		
+	}
+
 	return (
 		<div className = 'min-h-screen bg-cover bg-no-repeat flex items-center
 		justify-center' >
 			<div className = 'w-5/6 max-w-2xl backdrop-blur-2xl text-gray-300 border-2
 				border-gray-600 flex items-center justify-between max-sm:flex-col-reverse
 				rounded-lg'>
-				<form className = 'flex flex-col gap-5 p-10 flex-1' >
+				<form onSubmit = {handleSubmit} className = 'flex flex-col gap-5 p-10 flex-1' >
 					<h3 className = 'text-lg' >Profile details</h3>
 					<label htmlFor = "avatar" className = 'flex items-center gap-3
 						cursor-pointer' >
@@ -41,7 +46,8 @@ const ProfilePage = () => {
 						</button>
 
 				</form>
-				<img src = "" alt = "" />
+				<img className = 'max-w-44 aspect-square rounded-full mx-10 max-sm:mt-10' 
+					src = {assets.logo_icon} alt = "" />
 			</div>
 		</div>
 	)
