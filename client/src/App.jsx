7482b4386a73dedc24a4eb8b0d68e34681ea412c
abhishekmail-prog <abhishekmail0301@gmail.com>
2026-06-26 18:20:18 +0530

    Setup backend project with Express and core dependencies

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,8 @@ import ProfilePage from './pages/ProfilePage'
 const App = () => {
   return ( 
 
-    <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
+    <div className="bg-[url('./src/assets/bgImage.svg')] 
+      bg-contain">
       <Routes>
         <Route path = '/' element= {<HomePage />}/>
         <Route path = '/login' element= {<LoginPage />}/>
