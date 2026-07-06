dfc34fdf285f5f9e54b18132d0b5ef9acf037043
abhishekmail-prog <abhishekmail0301@gmail.com>
2026-07-06 18:42:48 +0530

    feat(auth): add authentication check on app initialization

--- client/context/AuthContext.jsx
+++ client/context/AuthContext.jsx
@@ -1,5 +1,6 @@
 import { createContext } from "react";
 import axios from 'axios';
+import toast from 'react-hot-toast';
 
 const backendUrl = import.meta.env.VITE_BACKEND_URL;
 axios.defaults.baseURL= backendUrl;
@@ -12,6 +13,21 @@ export const AuthProvider = ({ children })=>{
 	const [authUser, setAuthUser] = useState(null);
 	const [onlineUsers, setOnlineUsers] = useState([]);
 	const [socket, setSocket] = useState(null);
+
+	// Check if user is authenticated and if so, 
+	// set the user data and connect the socket
+	const checkAuth = async () => {
+		try {
+			const { data } = await axios.get("/api/auth/check");
+			if (data.success) {
+				setAuthUser(data.user)
+			}
+
+		} catch(error) {
+			toast.error(error.message)
+		}
+	}
+
 
 	const value = {
 		axios,
